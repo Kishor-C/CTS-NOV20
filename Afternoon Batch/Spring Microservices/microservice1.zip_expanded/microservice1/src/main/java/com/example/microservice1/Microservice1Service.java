cee2508ecd3c133557256c26422e594c9b9c6631
Kishor <kishor1124@gmail.com>
2020-12-09 15:36:16 +0530

    zipking & sleuth code

--- Afternoon Batch/Spring Microservices/microservice1.zip_expanded/microservice1/src/main/java/com/example/microservice1/Microservice1Service.java
+++ Afternoon Batch/Spring Microservices/microservice1.zip_expanded/microservice1/src/main/java/com/example/microservice1/Microservice1Service.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class Microservice1Service {
